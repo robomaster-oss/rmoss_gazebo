2c488c49bb980fe061d1c2ca095932ca1c86349d
zhenpeng ge <zhenpeng.ge@qq.com>
2021-01-09 23:30:36 +0800

    update ProjectileShooter,it works well

--- rmoss_ign_plugins/src/projectile_shooter/ProjectileShooter.cpp
+++ rmoss_ign_plugins/src/projectile_shooter/ProjectileShooter.cpp
@@ -18,16 +18,18 @@
 #include <ignition/plugin/Register.hh>
 #include <ignition/transport/Node.hh>
 
+#include "ignition/gazebo/components/ContactSensorData.hh"
+#include "ignition/gazebo/components/Name.hh"
+#include "ignition/gazebo/components/ParentEntity.hh"
+#include <ignition/gazebo/components/LinearVelocityCmd.hh>
+#include <ignition/gazebo/components/Pose.hh>
+#include <ignition/gazebo/components/World.hh>
+
 #include <ignition/gazebo/Conversions.hh>
 #include <ignition/gazebo/Link.hh>
 #include <ignition/gazebo/Model.hh>
 #include <ignition/gazebo/SdfEntityCreator.hh>
 #include <ignition/gazebo/Util.hh>
-#include <ignition/gazebo/components/AngularVelocity.hh>
-#include <ignition/gazebo/components/LinearVelocity.hh>
-#include <ignition/gazebo/components/LinearVelocityCmd.hh>
-#include <ignition/gazebo/components/Pose.hh>
-#include <ignition/gazebo/components/World.hh>
 
 #include <ignition/math/Pose3.hh>
 #include <ignition/math/Quaternion.hh>
@@ -66,18 +68,21 @@ public:
     void PostUpdate(const ignition::gazebo::UpdateInfo& _info, const ignition::gazebo::EntityComponentManager& _ecm);
 
 public:
+    // node and tool
     transport::Node node;
+    transport::Node::Publisher attackPub;
     std::unique_ptr<SdfEntityCreator> creator { nullptr };
-    Entity worldEntity { kNullEntity };
-    //current model and shooter link
-    Model model { kNullEntity };
-    Entity shooterEntity { kNullEntity };
-    //general data
+    bool initialized { false };
+    // entity: world,model, shooter link
+    Entity world { kNullEntity };
+    Entity model { kNullEntity };
+    Entity shooterLink { kNullEntity };
     std::string modelName;
-    std::string shooterName { "shooter" };
-    sdf::Root projectileSdfRoot;
+    std::string shooterName;
+    math::Pose3d shooterOffset;
+    // projectile
+    sdf::Model projectileSdfModel;
     unsigned int projectileId = 0;
-    //control data
     double projectileVel { 20 };
     int currentTotalNum { 1000000 };
     int waitShootNum { 0 };
@@ -96,50 +101,74 @@ void ProjectileShooter::Configure(const Entity& _entity,
     EntityComponentManager& _ecm,
     EventManager& _eventMgr)
 {
-    this->dataPtr->model = Model(_entity);
-    if (!this->dataPtr->model.Valid(_ecm)) {
+    this->dataPtr->model = _entity;
+    auto modelWrapper = Model(_entity);
+    if (!modelWrapper.Valid(_ecm)) {
         ignerr << "ProjectileShooter plugin should be attached to a model entity. Failed to initialize." << std::endl;
         return;
     }
+    this->dataPtr->modelName = modelWrapper.Name(_ecm);
     // Get params from SDF
+    auto linkName= _sdf->Get<std::string>("shooter_link");
+    this->dataPtr->shooterLink = modelWrapper.LinkByName(_ecm, linkName);
+    if (this->dataPtr->shooterLink == kNullEntity) {
+        ignerr << "shooter link with name[" << linkName << "] not found. " << std::endl;
+        return;
+    }
     if (_sdf->HasElement("shooter_name")) {
         this->dataPtr->shooterName = _sdf->Get<std::string>("shooter_name");
+    }
+    this->dataPtr->shooterOffset = math::Pose3d(0.2, 0, 0, 0, 0, 0);
+    if (_sdf->HasElement("shooter_offset")) {
+        this->dataPtr->shooterOffset = _sdf->Get<math::Pose3d>("shooter_offset");
     }
     if (_sdf->HasElement("projectile_velocity")) {
         this->dataPtr->projectileVel = _sdf->Get<double>("projectile_velocity");
     }
-    // Get shooter link
-    auto linkName = _sdf->Get<std::string>("shooter_link");
-    this->dataPtr->shooterEntity = this->dataPtr->model.LinkByName(_ecm, linkName);
-    if (this->dataPtr->shooterEntity == kNullEntity) {
-        ignerr << "shooter link with name[" << linkName << "] not found. " << std::endl;
+    if (_sdf->HasElement("projectile_num")) {
+        this->dataPtr->currentTotalNum = _sdf->Get<double>("projectile_num");
+    }
+    std::string projectile_uri;
+    if (_sdf->HasElement("projectile_uri")) {
+        projectile_uri = _sdf->Get<std::string>("projectile_uri");
+    } else {
+        ignerr << "The tag <projectile_uri> is not found." << std::endl;
         return;
     }
+    std::string attackTopic = "/referee_system/attack_info";
+    if (_sdf->HasElement("attack_topic")) {
+        attackTopic = _sdf->Get<std::string>("attack_topic");
+    }
     // Load projectile model sdf
-    std::string uri = "model://rm_projectile17/model.sdf";
     ignition::common::SystemPaths systemPaths;
-    auto model_path = systemPaths.FindFileURI(uri);
-    sdf::Errors errors = this->dataPtr->projectileSdfRoot.Load(model_path);
+    sdf::Root projectileSdfRoot;
+    sdf::Errors errors = projectileSdfRoot.Load(systemPaths.FindFileURI(projectile_uri));
     if (!errors.empty()) {
         for (const auto& e : errors) {
             ignerr << e.Message() << std::endl;
         }
         return;
     }
-    if (this->dataPtr->projectileSdfRoot.ModelCount() == 0) {
+    if (projectileSdfRoot.ModelCount() == 0) {
         ignerr << "Projectile Model not found" << std::endl;
         return;
     }
-    ignmsg << "load projectile name:[" << this->dataPtr->projectileSdfRoot.ModelByIndex(0)->Name() << "]" << std::endl;
-    this->dataPtr->modelName = this->dataPtr->model.Name(_ecm);
+    this->dataPtr->projectileSdfModel = *projectileSdfRoot.ModelByIndex(0);
     // Subscribe to commands
-    std::string topic { this->dataPtr->modelName + "/" + this->dataPtr->shooterName + "/shoot" };
-    this->dataPtr->node.Subscribe(topic, &ProjectileShooterPrivate::OnCmd, this->dataPtr.get());
-    ignmsg << "[" << this->dataPtr->shooterName << "] subscribing to shoot messages on [" << topic << "]" << std::endl;
-    //publish status of shooter
-    //creator of projectile
+    std::string shootTopic { this->dataPtr->modelName + "/" + this->dataPtr->shooterName + "/shoot" };
+    this->dataPtr->node.Subscribe(shootTopic, &ProjectileShooterPrivate::OnCmd, this->dataPtr.get());
+    this->dataPtr->attackPub = this->dataPtr->node.Advertise<msgs::StringMsg>(attackTopic);
+    //creator and world
     this->dataPtr->creator = std::make_unique<SdfEntityCreator>(_ecm, _eventMgr);
-    this->dataPtr->worldEntity = _ecm.EntityByComponents(components::World());
+    this->dataPtr->world = _ecm.EntityByComponents(components::World());
+    this->dataPtr->initialized = true;
+    //debug info
+    igndbg << "[" << this->dataPtr->modelName << " ProjectileShooter info]:" << std::endl;
+    igndbg << "shooter name: " << this->dataPtr->shooterName << std::endl;
+    igndbg << "shooter offset: " << this->dataPtr->shooterOffset << std::endl;
+    igndbg << "projectile name: " << projectileSdfRoot.ModelByIndex(0)->Name() << std::endl;
+    igndbg << "shoot topic: " << shootTopic << std::endl;
+    igndbg << "attack topic: " << attackTopic << std::endl;
 }
 
 void ProjectileShooter::PreUpdate(const ignition::gazebo::UpdateInfo& _info,
@@ -159,75 +188,55 @@ void ProjectileShooterPrivate::OnCmd(const ignition::msgs::Int32& _msg)
 {
     std::lock_guard<std::mutex> lock(this->waitShootNumMutex);
     this->waitShootNum = _msg.data();
-    ignmsg << "ProjectileShooter msg x: [" << _msg.data() << "]" << std::endl;
+    //ignmsg << "ProjectileShooter msg x: [" << _msg.data() << "]" << std::endl;
 }
 
-int i = 0;
 void ProjectileShooterPrivate::PreUpdate(const ignition::gazebo::UpdateInfo& _info, ignition::gazebo::EntityComponentManager& _ecm)
 {
-    //do nothing if paused.
-    if (_info.paused) {
+    //do nothing if it's paused or not initialized.
+    if (_info.paused || !this->initialized) {
         return;
     }
-    //for test
-    i++;
-    if (i == 4000) {
-        this->waitShootNum = 1;
-    } else if (i % 4000 == 0) {
-        this->waitShootNum = 1;
-    }
-    //control for chassis
-    Link shooterEntity(this->shooterEntity);
-    if (!_ecm.Component<components::WorldPose>(this->shooterEntity)) {
-        _ecm.CreateComponent(this->shooterEntity, components::WorldPose());
-    }
-    if (!_ecm.Component<components::LinearVelocity>(this->shooterEntity)) {
-        _ecm.CreateComponent(this->shooterEntity, components::LinearVelocity());
-    }
-    if (!_ecm.Component<components::AngularVelocity>(this->shooterEntity)) {
-        _ecm.CreateComponent(this->shooterEntity, components::AngularVelocity());
+    //Pose of shooter Link
+    if (!_ecm.Component<components::WorldPose>(this->shooterLink)) {
+        _ecm.CreateComponent(this->shooterLink, components::WorldPose());
     }
     // current state
-    const auto shooterPose = _ecm.Component<components::WorldPose>(this->shooterEntity)->Data();
-    const auto linearVel = _ecm.Component<components::LinearVelocity>(this->shooterEntity)->Data();
-    const auto angularVel = _ecm.Component<components::AngularVelocity>(this->shooterEntity)->Data();
-    // check spawn CMD
-    bool spawnFlag = true;
-    if (this->currentTotalNum < 0) {
-        spawnFlag = false;
-    }
-    // wait the last projectile init completely
-    if (!this->spawnedProjectiles.empty()) {
-        ProjectileInfo& last = this->spawnedProjectiles.back();
-        double t = std::chrono::duration_cast<std::chrono::milliseconds>(_info.simTime - last.spawnTime).count();
-        if (last.isInit && t > 100) {
-            spawnFlag = true;
-        } else {
-            spawnFlag = false;
-        }
-    } else {
-        spawnFlag = true;
-    }
-    if (spawnFlag) {
+    const auto shooterPose = _ecm.Component<components::WorldPose>(this->shooterLink)->Data();
+    // spawnFlag CMD
+    bool spawnFlag = false;
+    {
         std::lock_guard<std::mutex> lock(this->waitShootNumMutex);
-        if (this->waitShootNum <= 0) {
-            spawnFlag = false;
+        if (this->currentTotalNum > 0 && this->waitShootNum > 0) {
+            if (!this->spawnedProjectiles.empty()) {
+                //the last projectile finished to init.
+                ProjectileInfo& last = this->spawnedProjectiles.back();
+                double t = std::chrono::duration_cast<std::chrono::milliseconds>(_info.simTime - last.spawnTime).count();
+                if (last.isInit && t > 100) {
+                    spawnFlag = true;
+                }
+            } else {
+                //the first projectile
+                spawnFlag = true;
+            }
         }
     }
     //process projectile
     if (spawnFlag) {
-        //spawn a projectiles
+        //spawn a projectile
         auto projectileName = this->modelName + "_" + this->shooterName + "_" + std::to_string(this->projectileId);
-        math::Pose3d shooterOffset(0.2, 0, 0, 0, 0, 0);
-        sdf::Model modelToSpawn = *this->projectileSdfRoot.ModelByIndex(0);
-        modelToSpawn.SetName(projectileName);
-        modelToSpawn.SetRawPose(shooterPose * shooterOffset);
-        Entity entity = this->creator->CreateEntities(&modelToSpawn);
-        this->creator->SetParent(entity, this->worldEntity);
-        //set velocity
+        this->projectileSdfModel.SetName(projectileName);
+        this->projectileSdfModel.SetRawPose(shooterPose * this->shooterOffset);
+        Entity projectileModel = this->creator->CreateEntities(&this->projectileSdfModel);
+        this->creator->SetParent(projectileModel, this->world);
+        //update projectile,set velocity and create ContactSensorData
         math::Vector3d tmpVel(this->projectileVel, 0, 0);
-        _ecm.CreateComponent(entity, components::LinearVelocityCmd({ tmpVel }));
-        ProjectileInfo pInfo(entity, projectileName, _info.simTime);
+        _ecm.CreateComponent(projectileModel, components::LinearVelocityCmd({ tmpVel }));
+        Entity projectileLink = Model(projectileModel).Links(_ecm)[0];
+        Entity projectileCollision = Link(projectileLink).Collisions(_ecm)[0];
+        _ecm.CreateComponent(projectileCollision, components::ContactSensorData());
+        //update queue and counters
+        ProjectileInfo pInfo(projectileModel, projectileName, _info.simTime);
         this->spawnedProjectiles.push(pInfo);
         this->projectileId++;
         {
@@ -236,28 +245,66 @@ void ProjectileShooterPrivate::PreUpdate(const ignition::gazebo::UpdateInfo& _in
         }
         this->currentTotalNum--;
     } else {
-        //try to init velocity
+        //try to init the last projectile
         if (!this->spawnedProjectiles.empty()) {
             ProjectileInfo& pInfo = this->spawnedProjectiles.back();
             if (!pInfo.isInit) {
+                //cancel velocity
                 _ecm.RemoveComponent<components::LinearVelocityCmd>(pInfo.entity);
                 pInfo.isInit = true;
             }
         }
     }
-    //check time and delete projectiles
+    //check cantact and time to delete projectiles
     if (!this->spawnedProjectiles.empty()) {
         ProjectileInfo& pInfo = this->spawnedProjectiles.front();
-        double t = std::chrono::duration_cast<std::chrono::milliseconds>(_info.simTime - pInfo.spawnTime).count();
-        if (pInfo.isInit && t > 5000) {
-            this->creator->RequestRemoveEntity(pInfo.entity);
-            this->spawnedProjectiles.pop();
+        if (pInfo.isInit) {
+            Entity link = Model(pInfo.entity).Links(_ecm)[0];
+            Entity collision = Link(link).Collisions(_ecm)[0];
+            auto contacts = _ecm.Component<components::ContactSensorData>(collision)->Data();
+            if (contacts.contact_size() > 0) {
+                Entity collision1 = contacts.contact(0).collision1().id();
+                Entity collision2 = contacts.contact(0).collision2().id();
+                Entity targetCollision = (collision == collision1) ? collision2 : collision1;
+                // TODO (maybe)
+                // choose the best one according to some rules(for example,contain substring 'target') if the contact_size > 1 .
+                // if (contacts.contact_size() > 1) {
+                //     for (int i = 0; i < contacts.contact_size(); i++) {
+                //         collision1 = contacts.contact(i).collision1().id();
+                //         collision2 = contacts.contact(i).collision2().id();
+                //         auto tmpCollision = (collision == collision1) ? collision2 : collision1;
+                //         std::string name = *_ecm.ComponentData<components::Name>(tmpCollision);
+                //         if(name.find("target")!=name.npos){
+                //             targetCollision=tmpCollision;
+                //             break;
+                //         }
+                //     }
+                // }
+                std::string result;
+                auto name1 = scopedName(this->shooterLink, _ecm);
+                auto name2 = scopedName(targetCollision, _ecm);
+                result.insert(0, name2);
+                result.insert(0, ">");
+                result.insert(0, name1);
+                // publish msg
+                msgs::StringMsg msg;
+                msg.mutable_header()->mutable_stamp()->CopyFrom(convert<msgs::Time>(_info.simTime));
+                msg.set_data(result);
+                this->attackPub.Publish(msg);
+                //ignmsg << "ProjectileShooter contact_size: [" << contacts.contact_size() << "]" << std::endl;
+                //ignmsg << "ProjectileShooter scop : [" << result << "]" << std::endl;
+            }
+            double t = std::chrono::duration_cast<std::chrono::milliseconds>(_info.simTime - pInfo.spawnTime).count();
+            if (contacts.contact_size() > 0 || t > 4000) {
+                this->creator->RequestRemoveEntity(pInfo.entity);
+                this->spawnedProjectiles.pop();
+            }
         }
     }
 }
 
-void ProjectileShooterPrivate::PostUpdate(const ignition::gazebo::UpdateInfo& _info,
-    const ignition::gazebo::EntityComponentManager& _ecm)
+void ProjectileShooterPrivate::PostUpdate(const ignition::gazebo::UpdateInfo& /*_info*/,
+    const ignition::gazebo::EntityComponentManager& /*_ecm*/)
 {
 }
 
