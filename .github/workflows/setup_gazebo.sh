b3ec532334d65025fbb958d498aaed83f326ce82
zhenpeng ge <zhenpeng.ge@qq.com>
2022-11-14 23:30:02 +0800

    fix ci

--- .github/workflows/setup_gazebo.sh
+++ .github/workflows/setup_gazebo.sh
@@ -4,5 +4,5 @@ echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-prerelease `lsb_releas
 echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
 wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
 apt-get update -qq
-apt-get install -y libignition-msgs8-dev libignition-transport11-dev libignition-gazebo6-dev
+apt-get install -y libignition-cmake2-dev libignition-msgs8-dev libignition-transport11-dev libignition-gazebo6-dev
 apt-get install -y ros-humble-ros-ign 
